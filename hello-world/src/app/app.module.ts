71b24e1f8f61ff1e2c5a8ba82caacb3561f8305f
arjun <arjunporwal10@gmail.com>
2018-08-16 11:48:00 +0530

    two way binding

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
