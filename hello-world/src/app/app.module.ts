a339a67507e519f1fb6bea353af0549abb1370c5
arjun <arjunporwal10@gmail.com>
2018-08-16 15:01:17 +0530

    pipes

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
-import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -11,8 +11,7 @@ import {FormsModule} from '@angular/forms';
     TestComponent,
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-test',
   template: `
-  <input [(ngModel)]="name" type ="text">
-  {{name}}
+  <h2>{{name}}</h2>
+  <h2>{{name | lowercase}}</h2>
+  <h2>{{message | titlecase}}</h2>
+  <h2>{{name | slice:3}}</h2>
+  <h2>{{person | json}}</h2>
+  <h2>{{5.678 | number:'1.2-3'}}</h2>
+  <h2>{{5.678 | number:'3.4-5'}}</h2>
+  <h2>{{5.678 | number:'3.1-2'}}</h2>
+  <h2>{{0.25 | percent}}</h2>
+  <h2>{{0.25 | currency:'INR'}}</h2>
+  <h2>{{date}}</h2>
+  <h2>{{date | date:'short'}}</h2>
   `,
   styles: [`  `]
 })
 export class TestComponent implements OnInit {
 
-  public name = "";
-  public successClass = "text-success";
-  constructor() { }
 
+
+  constructor() { }
+  public name = "Codevolution";
+  public message = "Welcom to codevolution";
+  public person = {
+    "firstName" : "John",
+    "lastName" : "Doe"
+  }
+  public date = new Date();
   ngOnInit() {
   }
 
